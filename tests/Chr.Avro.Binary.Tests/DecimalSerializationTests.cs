5330a3c4aa58ed449f36728f2343c9e5b27225b0
Dan Stelljes <dan.stelljes@chrobinson.com>
2019-09-18 09:25:09 -0500

    Add additional decimal encoding tests

--- tests/Chr.Avro.Binary.Tests/DecimalSerializationTests.cs
+++ tests/Chr.Avro.Binary.Tests/DecimalSerializationTests.cs
@@ -17,52 +17,34 @@ namespace Chr.Avro.Serialization.Tests
         }
 
         [Theory]
-        [MemberData(nameof(Resizings))]
-        public void ResizedDecimalValues(int precision, int scale, decimal value, decimal rounded)
+        [MemberData(nameof(Encodings))]
+        public void EncodedDecimalValues(int precision, int scale, decimal value, byte[] encoding, decimal resizing)
         {
             var schema = new BytesSchema()
             {
                 LogicalType = new DecimalLogicalType(precision, scale)
             };
 
-            var deserializer = DeserializerBuilder.BuildDeserializer<decimal>(schema);
-            var serializer = SerializerBuilder.BuildSerializer<decimal>(schema);
-
-            Assert.Equal(rounded, deserializer.Deserialize(serializer.Serialize(value)));
-        }
-
-        [Theory]
-        [MemberData(nameof(ZeroScaleEncodings))]
-        public void ZeroScaleDecimalValues(decimal value, byte[] encoding)
-        {
-            var schema = new BytesSchema()
-            {
-                LogicalType = new DecimalLogicalType(12, 0)
-            };
-
             var serializer = SerializerBuilder.BuildSerializer<decimal>(schema);
             Assert.Equal(encoding, serializer.Serialize(value));
 
             var deserializer = DeserializerBuilder.BuildDeserializer<decimal>(schema);
-            Assert.Equal(value, deserializer.Deserialize(encoding));
+            Assert.Equal(resizing, deserializer.Deserialize(encoding));
         }
 
-        public static IEnumerable<object[]> Resizings => new List<object[]>
+        public static IEnumerable<object[]> Encodings => new List<object[]>
         {
-            new object[] { 1, 0, 404.04m, 404m },
-            new object[] { 3, 0, 404.04m, 404m },
-            new object[] { 5, 0, 404.0404m, 404m },
-            new object[] { 5, 2, -404.0m, -404.00m },
-            new object[] { 5, 4, -404.040404m, -404.0404m },
-        };
-
-        public static IEnumerable<object[]> ZeroScaleEncodings => new List<object[]>
-        {
-            new object[] { -32768m, new byte[] { 0x04, 0x80, 0x00 } },
-            new object[] { -1m, new byte[] { 0x02, 0xff } },
-            new object[] { -0m, new byte[] { 0x02, 0x00 } },
-            new object[] { 1m, new byte[] { 0x02, 0x01 } },
-            new object[] { 32767m, new byte[] { 0x04, 0x7f, 0xff } },
+            new object[] { 6, 0, -32769m, new byte[] { 0x06, 0xff, 0x7f, 0xff }, -32769m },
+            new object[] { 6, 0, -32768m, new byte[] { 0x04, 0x80, 0x00 }, -32768m },
+            new object[] { 5, 2, -1666.6666m, new byte[] { 0x06, 0xfd, 0x74, 0xf6 }, -1666.66m },
+            new object[] { 4, 0, -1m, new byte[] { 0x02, 0xff }, -1m },
+            new object[] { 6, 5, -0.125, new byte[] { 0x04, 0xcf, 0x2c }, -0.12500 },
+            new object[] { 2, 0, -0m, new byte[] { 0x02, 0x00 }, 0m },
+            new object[] { 6, 5, 0.125, new byte[] { 0x04, 0x30, 0xd4 }, 0.12500 },
+            new object[] { 4, 0, 1m, new byte[] { 0x02, 0x01 }, 1m },
+            new object[] { 5, 2, 1666.6666m, new byte[] { 0x06, 0x02, 0x8b, 0x0a }, 1666.66m },
+            new object[] { 6, 0, 32767m, new byte[] { 0x04, 0x7f, 0xff }, 32767m },
+            new object[] { 6, 0, 32768m, new byte[] { 0x06, 0x00, 0x80, 0x00 }, 32768m },
         };
     }
 }
