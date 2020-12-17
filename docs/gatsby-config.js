87619fde5dff491593c1a0b03c14623bafdee97f
Dan Stelljes <dan.stelljes@chrobinson.com>
2020-12-17 09:55:54 -0600

    Release 7.0.3

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = {
   siteMetadata: {
     githubUrl: 'https://github.com/ch-robinson/dotnet-avro',
-    latestRelease: '7.0.2',
+    latestRelease: '7.0.3',
     projectName: 'Chr.Avro'
   },
   pathPrefix: '/dotnet-avro',
