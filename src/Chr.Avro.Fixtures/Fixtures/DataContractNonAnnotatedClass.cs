ebd0599c1d44c1526d8dc4a957a499bd1bad00d7
Dan Stelljes <dan.stelljes@chrobinson.com>
2023-11-03 13:26:02 -0500

    Support `IgnoreDataMember` attribute (#287)

--- src/Chr.Avro.Fixtures/Fixtures/DataContractNonAnnotatedClass.cs
+++ src/Chr.Avro.Fixtures/Fixtures/DataContractNonAnnotatedClass.cs
@@ -15,8 +15,11 @@ namespace Chr.Avro.Fixtures
         [DataMember(Name = "DifferentField", Order = 5)]
         public int AnnotatedCustomField;
 
-        [NonSerialized]
+        [IgnoreDataMember]
         public int IgnoredField;
+
+        [NonSerialized]
+        public int NonSerializedField;
 
         [DataMember]
         [NonSerialized]
@@ -29,5 +32,8 @@ namespace Chr.Avro.Fixtures
 
         [DataMember(Name = "DifferentProperty", Order = 1)]
         public int AnnotatedCustomProperty { get; set; }
+
+        [IgnoreDataMember]
+        public int IgnoredProperty { get; set; }
     }
 }
